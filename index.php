7bab06a743e080f274482395be911fcb8f2bfa4b
Pramod <pramod.student@acme.edu.np>
2022-03-29 22:44:32 +0545

    search-box

--- index.php
+++ index.php
@@ -1,3 +1,19 @@
+<!DOCTYPE html>
 <html>
+
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Php cURL</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+    <div class="container">
+        <form action="" class="search-bar">
+            <input type="text" placeholder="Search Here" name="search">
+            <button type="submit"><img src="images/search.png"></button>
+        </form>
+    </div>
+</body>
 
 </html>
